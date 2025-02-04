87870f6519581b86cfa162482529dc4c7e4e879d
John Bell <jan@altenter.org>
2025-02-04 09:12:43 +0100

    ADD a service

---
+++ Service/MainService.py
@@ -0,0 +1,5 @@
+class MainService:
+    my_options =["My Opt 1", "My Opt 2", "My Opt 3"]
+
+    def get_options(self):
+        return self.my_options
--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-# Minimum app.py for debugging -->>
 
-from flask import Flask
+from flask import Flask, render_template
+
+from Service.MainService import MainService
 
 app = Flask(__name__)
 
+m_main_service = MainService()
+
 @app.route("/")
 def home():
-    return "<h1>hello my flask server test</h1>"
+    return render_template('base.html', service = m_main_service)
+
+@app.route('/hello')
+def hello():
+    return 'Hello, App'
 
 if __name__ == "__main__":
     app.run(debug=True)
-# <<--
+
